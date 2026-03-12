9e1c8c391c001ba04d0beea537461907a64ba78c
Rahul Kumar <rahulkumar8340527941@gmail.com>
2026-03-12 11:57:45 +0530

    image fixed

--- client/restaurant/src/components/Navbar.jsx
+++ client/restaurant/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ search, setSearch }) => {
       <img src="/assets/Icon button.png" alt="Restaurant Logo" />
       <div className="search-container">
         <input className='navSearch' type="text" placeholder='Filter...' onChange={(e) => { setSearch(e.target.value) }} value={search} />
-        <img className="dropdown" src="/src/assets/Frame 3.png" alt="" />
+        <img className="dropdown" src="/assets/Frame 3.png" alt="" />
       </div>
     </div>
   )
